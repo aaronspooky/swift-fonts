c5f93ceabff51a0735a5abdc1fc0531e30ce5d98
Sergio A. Balderas <sergioa.balderas@Mac-mini-de-Administrador.local>
2017-07-03 15:25:38 -0500

    Font size

--- Fonts/FontSizesViewController.swift
+++ Fonts/FontSizesViewController.swift
@@ -20,11 +20,6 @@ class FontSizesViewController: UITableViewController {
       tableView.estimatedRowHeight = FontSizesViewController.pointSize[0]
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     // MARK: - Table view data source
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
